7af3cd61694045a7aaa45163013fcdb9e8c9c146
yuudi <yuudi@users.noreply.github.com>
2022-03-17 15:54:15 -0400

    unify

---
+++ build-all.sh
@@ -0,0 +1,16 @@
+grunt --lang=en && mv _site site/en
+grunt --lang=de && mv _site site/de
+grunt --lang=es && mv _site site/es
+grunt --lang=fa && mv _site site/fa
+grunt --lang=fr && mv _site site/fr
+grunt --lang=hy && mv _site site/hy
+grunt --lang=it && mv _site site/it
+grunt --lang=ja && mv _site site/ja
+grunt --lang=ko && mv _site site/ko
+grunt --lang=nl && mv _site site/nl
+grunt --lang=pt-br && mv _site site/pt-br
+grunt --lang=pt-pt && mv _site site/pt-pt
+grunt --lang=ru && mv _site site/ru
+grunt --lang=zh && mv _site site/zh
+grunt --lang=zh-tw && mv _site site/zh-tw
+grunt --lang=ar && mv _site site/ar
